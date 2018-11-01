81320caa8b3af2e822e0648af5822307cd1c8209
Henry <hanke.liu@gmail.com>
2018-11-01 00:29:02 -0400

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class CashRegister
 
   def initialize(discount = 0)
@@ -8,8 +9,27 @@ class CashRegister
   def discount
     @discount
   end
-  
+
   def total
     @total
+  end
+
+  def add_item(title, price, quantity = 1)
+    @total += price * quantity
+    @quantity = quantity
+  end
+
+  def apply_discount
+    #binding.pry
+    @total = @total * (100 - @discount)/100 #20%
+    #binding.pry
+  end
+
+  def items
+    #items << title
+  end
+
+  def void_last_transaction
+    #self.total = 0
   end
 end
